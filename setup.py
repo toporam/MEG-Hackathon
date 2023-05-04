688bc6561dada105cb65e5615bb657e0bf50ece6
AmaiaBA <74140759+AmaiaBA@users.noreply.github.com>
2023-05-04 17:22:26 -0400

    create setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='MEG-Hackathon_Eyetracking',
+    version='1.0',
+    description='Python3 tools for eyetracking analysis',
+    author='LT, JS, SB, RK, ABA',
+    author_email='',
+    install_requires=['mne', 'numpy', 'scipy', 'pandas', 'matplotlib']
